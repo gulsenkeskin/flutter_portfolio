f0fc46def104eac0bc87e7da682ade794cd75116
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-05 23:11:46 +0300

    Create color_constants.dart

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  MyColors._();
+
+  static const Color salmonSalt = Color(0xffe99287);
+  static const Color mintJelly = Color(0xff43d9ad);
+  static const Color savoyBlue = Color(0xff4d5bce);
+  static const Color cropperBlue = Color(0xff607b96);
+  static const Color whisperBlue = Color(0xffe5e9f0);
+  static const Color blackKnight = Color(0xff010c15);
+  static const Color tangara = Color(0xff1e2d3d);
+  static const Color blackManga = Color(0xff011627);
+}
