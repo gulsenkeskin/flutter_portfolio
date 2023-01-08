2401600967cf5c757c2ee4ac5daf99543b7000dd
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-09 00:44:07 +0300

    Update color_constants.dart

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -5,10 +5,15 @@ class MyColors {
 
   static const Color salmonSalt = Color(0xffe99287);
   static const Color mintJelly = Color(0xff43d9ad);
+  static const Color lightMintJelly=Color(0xb243d9ad);
   static const Color savoyBlue = Color(0xff4d5bce);
   static const Color cropperBlue = Color(0xff607b96);
   static const Color whisperBlue = Color(0xffe5e9f0);
   static const Color blackKnight = Color(0xff010c15);
   static const Color tangara = Color(0xff1e2d3d);
   static const Color blackManga = Color(0xff011627);
+  static const Color blackIsBlack = Color(0xff0c1616);
+  static const Color ramsons=Color(0xb2175553);
+  static const Color peacesChrema=Color(0xfffea55f);
+
 }
