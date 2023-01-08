919ca39bc7dca436046347082b786a35630c9d90
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-09 00:43:56 +0300

    Update chess_game.dart

--- lib/view/home/widgets/chess_game.dart
+++ lib/view/home/widgets/chess_game.dart
@@ -10,57 +10,16 @@ class ChessGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ChessBoardController controller = ChessBoardController();
-
-    return Positioned(
-      left: context.fem980,
-      top: context.dynamicFem(130),//fem179
-      child: Align(
-        alignment: Alignment.center,
-        child: SizedBox(
-          width: context.fem780,
-          height: context.fem742,
-          //todo oyun alanı ---------
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ChessBoard(
-                size: context.fem521,
-                controller: controller,
-                boardColor: BoardColor.green,
-                boardOrientation: PlayerColor.white,
-              ),
-            ],
-          ), /*Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Expanded(
-                child: Center(
-                  child: ChessBoard(
-                    size: 800,
-                    controller: controller,
-                    boardColor: BoardColor.green,
-                    boardOrientation: PlayerColor.white,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: ValueListenableBuilder<Chess>(
-                  valueListenable: controller,
-                  builder: (context, game, _) {
-                    return Text(
-                      controller.getSan().fold(
-                            '',
-                            (previousValue, element) =>
-                                previousValue + '\n' + (element ?? ''),
-                          ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),*/
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ChessBoard(
+          size: context.fem475,
+          controller: controller,
+          boardColor: BoardColor.green,
+          boardOrientation: PlayerColor.white,
         ),
-      ),
+      ],
     );
   }
 }
